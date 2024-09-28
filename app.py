0b2acd240c9bc3bbd4e1ad0715d644dd99816854
Sriyan Madugula <sriyan@umich.edu>
2024-09-28 15:03:16 -0400

    groq speech to text

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 from groq import Groq
 
 # Initialize the Groq client
-client = Groq()
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
 
 # Specify the path to the audio file
-filename = os.path.dirname(__file__) + "/sample_audio.m4a" # Replace with your audio file!
+filename = os.path.dirname(__file__) + "/BST.mp4" # Replace with your audio file!
 
 # Open the audio file
 with open(filename, "rb") as file:
@@ -13,9 +15,10 @@ with open(filename, "rb") as file:
     translation = client.audio.translations.create(
       file=(filename, file.read()), # Required audio file
       model="whisper-large-v3", # Required model to use for translation
-      prompt="Specify context or spelling",  # Optional
+      prompt="Make sure all filler words are included",  # Optional
       response_format="json",  # Optional
       temperature=0.0  # Optional
     )
     # Print the translation text
-    print(translation.text)
+    text = translation.text
+    print(text)
